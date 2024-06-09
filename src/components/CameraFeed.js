e7223d2481858b188f514b88da482a178a40ad5d
justin65 <ccfang@gmail.com>
2024-06-10 07:17:29 +0800

    revise layout

--- src/components/CameraFeed.js
+++ src/components/CameraFeed.js
@@ -1,8 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Webcam from 'react-webcam';
 
 const CameraFeed = ({ addPhoto }) => {
+  const [isMobile, setIsMobile] = useState(false);
   const webcamRef = useRef(null);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    setIsMobile(/iphone|ipad|ipod|android/.test(userAgent));
+  }, []);
 
   const capture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -11,12 +17,24 @@ const CameraFeed = ({ addPhoto }) => {
 
   return (
     <div>
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        width="100%"
-      />
+      {isMobile ? (
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width="100%"
+          videoConstraints={{
+            facingMode: { exact: "environment" }
+          }}
+        />
+      ) : (
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width="100%"
+        />
+      )}
       <button onClick={capture}>拍照</button>
     </div>
   );
