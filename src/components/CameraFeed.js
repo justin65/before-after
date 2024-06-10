03d1cc9584034de7df3153f78bcdd12fba69698d
justin65 <ccfang@gmail.com>
2024-06-10 10:56:58 +0800

    camera resolution test

--- src/components/CameraFeed.js
+++ src/components/CameraFeed.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import Webcam from 'react-webcam';
 
 const CameraFeed = ({ addPhoto, dimensions }) => {
+  const [videoConstraints, setVideoConstraints] = useState(null);
   const [isMobile, setIsMobile] = useState(false);
   const webcamRef = useRef(null);
 
@@ -9,6 +10,25 @@ const CameraFeed = ({ addPhoto, dimensions }) => {
     const userAgent = navigator.userAgent.toLowerCase();
     setIsMobile(/iphone|ipad|ipod|android/.test(userAgent));
   }, []);
+
+  useEffect(() => {
+    const getUserMedia = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+            const track = stream.getVideoTracks()[0];
+            const settings = track.getSettings();
+            setVideoConstraints({
+                width: settings.width,
+                height: settings.height,
+                facingMode: { exact: "environment" },
+            });
+        } catch (err) {
+            console.error('Error accessing user camera:', err);
+        }
+    };
+
+    getUserMedia();
+}, []);
 
   const capture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -29,7 +49,8 @@ const CameraFeed = ({ addPhoto, dimensions }) => {
           width={dimensions.width ? dimensions.width : 200}
           height={dimensions.height > 100 ? dimensions.height - 100 : 200}
           videoConstraints={{
-            facingMode: { exact: "environment" }
+            ...videoConstraints,
+            facingMode: { exact: "environment" },
           }}
         />
       ) : (
@@ -37,8 +58,9 @@ const CameraFeed = ({ addPhoto, dimensions }) => {
           audio={false}
           ref={webcamRef}
           screenshotFormat="image/jpeg"
-          width={dimensions.width ? dimensions.width : 200}
-          height={dimensions.height > 100 ? dimensions.height - 100 : 200}
+          width={dimensions.width ? dimensions.width : 100}
+          height={dimensions.height > 100 ? dimensions.height - 100 : 100}
+          videoConstraints={videoConstraints}
         />
       )}
       <button onClick={capture}>拍照</button>
